a8328d1cc7edc9ee1198166be56ecd40b2d5dec6
lemonnaid <bindalearning@gmail.com>
2024-07-03 22:10:57 +0545

    edits

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1>Gurkhatech Company Profile</h1>
+      <p>Gurkha Technology is a dynamic and forward-thinking company with a strong focus on digital marketing, graphics design, web design, and search engine optimization (SEO). We excel in crafting digital strategies that empower businesses to thrive in the digital landscape. </p>
+      <p>With a dedicated team of experts, we offer tailored solutions that elevate brands, drive online visibility, and engage audiences effectively.</p>
+      <p>Our expertise lies in creating captivating graphics, designing user-friendly websites, and implementing data-driven SEO strategies that propel businesses to new heights. As a one-stop solution provider, we are committed to helping our clients achieve their digital goals and establish a robust online presence.</p>
+
+      <p>Please click the below link to go through our company profile</p>
+
+      <NavLink to="https://gurkhatech.com/" className="profile">Company Profile</NavLink>
     </div>
   )
 }
--- src/pages/graphicdesign/Promotional.jsx
+++ src/pages/graphicdesign/Promotional.jsx
@@ -3,9 +3,16 @@ import React, { useState } from 'react';
 const PromoItems = () => {
   // Sample items with their prices
   const items = [
-    { id: 1, name: 'Logo Design Only', price: 15000 },
-    { id: 2, name: 'Logo Design with Complete Corporate Identity Package', price: 30000 },
-    { id: 3, name: 'Business Card', price: 2500 },
+    { id: 1, name: 'A4 Poster Design Double Side', price: 15000 },
+    { id: 2, name: 'Single Leaflet (3.9*8.3inches)', price: 7500 },
+    { id: 3, name: 'Bifold brochure(7.8*8.3inches)', price: 8500 },
+    { id: 4, name: 'Trifold Brochure (11.8*8.3inches) both side', price: 11000 },
+    { id: 5, name: 'Prospectus/CatalogueDesign (20 pages)', price: 30000 },
+    { id: 6, name: 'Dangler/Streamer/wobbler Design', price: 2500 },
+    { id: 7, name: 'Stickers', price: 1500 },
+    { id: 8, name: 'Calendars (Desk/Wall)', price: 5500 },
+    { id: 9, name: 'Shelf Strips', price: 1500 },
+    { id: 10, name: 'Table Tent Design', price: 2000 },
   ];
 
   // State to keep track of selected items
